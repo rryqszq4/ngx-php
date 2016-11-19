0d37bb93f69f99241ba842e85491944f4b4ad635
rryqszq4 <279664262@163.com>
2016-11-19 11:37:17 +0800

    [src] create php_ngx_test

---
+++ src/php/php_ngx_test.h
@@ -0,0 +1,23 @@
+/**
+ *    Copyright(c) 2016 rryqszq4
+ *
+ *
+ */
+
+#ifndef _PHP_NGX_TEST_H
+#define _PHP_NGX_TEST_H
+
+#include <ngx_http.h>
+
+#include <php.h>
+#include <php_ini.h>
+#include <ext/standard/info.h>
+
+PHP_METHOD(ngx_test_generator, __construct);
+PHP_METHOD(ngx_test_generator, yield);
+PHP_METHOD(ngx_test_generator, send);
+PHP_METHOD(ngx_test_generator, receive);
+
+void ngx_test_init(int module_number TSRMLS_DC);
+
+#endif
