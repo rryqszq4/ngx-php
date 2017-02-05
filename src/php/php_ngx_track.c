3d968c099265dbd6c587e0dcc97153d5b764acc9
rryqszq4 <279664262@163.com>
2017-02-05 18:55:02 +0800

    [src] add php_ngx_track.h, php_ngx_track.c

---
+++ src/php/php_ngx_track.c
@@ -0,0 +1,513 @@
+/**
+ *    Copyright(c) 2016 rryqszq4
+ *
+ *
+ */
+
+#include "php.h"
+#include "php_ini.h"
+#include "ext/standard/info.h"
+#include "zend_generators.h"
+#include "zend_vm.h"
+
+#include "php_ngx.h"
+#include "php_ngx_track.h"
+#include "../ngx_http_php_module.h"
+
+static int ngx_track_zval(zval zv);
+static void ngx_track_znode(unsigned int node_type, znode_op node, zend_op_array *op_array TSRMLS_DC);
+static void ngx_track_op_array(zend_op_array *op_array TSRMLS_DC);
+static int ngx_track_fe_wrapper(zval *el TSRMLS_DC, int num_args, va_list args, zend_hash_key *hash_key);
+static int ngx_track_cle_wrapper (zval *el TSRMLS_DC);
+//static int ngx_check_fe_wrapper (zval *el, zend_bool *have_fe TSRMLS_DC);
+static int ngx_track_fe(zend_op_array *fe TSRMLS_DC, int num_args, va_list args, zend_hash_key *hash_key);
+static int ngx_track_cle(zend_class_entry *class_entry TSRMLS_DC);
+//static int ngx_check_fe(zend_op_array *fe, zend_bool *have_fe TSRMLS_DC);
+static inline zend_class_entry *ngx_get_called_scope(const zend_execute_data *e);
+static inline const char *ngx_get_executed_filename(void);
+static void ngx_function_name(zend_execute_data *execute_data);
+
+zend_op_array* ngx_compile_file(zend_file_handle* file_handle, int type TSRMLS_DC)
+{
+    zend_op_array *op_array;
+
+    op_array = ori_compile_file(file_handle, type TSRMLS_CC);
+
+    ngx_http_request_t *r = ngx_php_request;
+    ngx_http_php_ctx_t *ctx;
+    ctx = ngx_http_get_module_ctx(r, ngx_http_php_module);
+
+    if (op_array && (ctx->output_type & OUTPUT_OPCODE)) {
+        ctx->output_type = OUTPUT_CONTENT;
+
+        //ngx_http_set_ctx(r, ctx, ngx_http_php_module);
+
+        if (!ctx->opcode_logo) {
+php_printf("    ___                   __    \n");
+php_printf("  /`__ \\___  ___  ___  __/ /__  \n");
+php_printf(" / /_/ / _ \\/ _ `/ _ \\/ _ / __\\ \n");
+php_printf(" \\___./ .__/\\___.\\___/\\___\\__..   /ngx_php7_tracker\n"); 
+php_printf("     /_/                         /version: %s\n", NGX_HTTP_PHP_MODULE_VERSION);
+php_printf("\n/* ~: IS_TMP_VAR, $: IS_VAR, !: IS_CV */\n\n");
+            ctx->opcode_logo = 1;
+        }
+
+        ngx_track_op_array(op_array TSRMLS_CC);
+
+        zend_hash_apply_with_arguments(CG(function_table) TSRMLS_CC, (apply_func_args_t) ngx_track_fe_wrapper, 0);
+    
+        zend_hash_apply(CG(class_table), (apply_func_t) ngx_track_cle_wrapper TSRMLS_CC);
+        
+        ctx->output_type = OUTPUT_OPCODE;
+
+        ngx_http_set_ctx(r, ctx, ngx_http_php_module);
+    }
+
+    //ctx->enable_output = 0;
+
+    //ngx_http_set_ctx(r, ctx, ngx_http_php_module);
+
+    return op_array;
+}
+
+zend_op_array *ngx_compile_string(zval *source_string, char *filename TSRMLS_DC)
+{
+    zend_op_array *op_array;
+
+    op_array = ori_compile_string(source_string, filename TSRMLS_CC);
+
+    ngx_http_request_t *r = ngx_php_request;
+    ngx_http_php_ctx_t *ctx;
+    ctx = ngx_http_get_module_ctx(r, ngx_http_php_module);
+
+    if (op_array && (ctx->output_type & OUTPUT_OPCODE)) {
+        ctx->output_type = OUTPUT_CONTENT;
+
+        //ngx_http_set_ctx(r, ctx, ngx_http_php_module);
+
+        if (!ctx->opcode_logo) {
+php_printf("    ___                   __    \n");
+php_printf("  /`__ \\___  ___  ___  __/ /__  \n");
+php_printf(" / /_/ / _ \\/ _ `/ _ \\/ _ / __\\ \n");
+php_printf(" \\___./ .__/\\___.\\___/\\___\\__..   /ngx_php7_tracker\n"); 
+php_printf("     /_/                         /version: %s\n", NGX_HTTP_PHP_MODULE_VERSION);
+php_printf("\n/* ~: IS_TMP_VAR, $: IS_VAR, !: IS_CV */\n\n");
+            ctx->opcode_logo = 1;
+        }
+
+        ngx_track_op_array(op_array TSRMLS_CC);
+
+        zend_hash_apply_with_arguments(CG(function_table) TSRMLS_CC, (apply_func_args_t) ngx_track_fe_wrapper, 0);
+    
+        zend_hash_apply(CG(class_table), (apply_func_t) ngx_track_cle_wrapper TSRMLS_CC);
+        
+        ctx->output_type = OUTPUT_OPCODE;
+
+        ngx_http_set_ctx(r, ctx, ngx_http_php_module);
+    }
+
+    //ctx->enable_output = 0;
+
+    //ngx_http_set_ctx(r, ctx, ngx_http_php_module);
+
+    return op_array;
+}
+
+static int ngx_track_zval(zval zv)
+{
+    switch (zv.u1.v.type) {
+        case IS_UNDEF: 
+            php_printf("%-16s","<undef>");
+            return IS_UNDEF;
+        case IS_NULL : 
+            php_printf("%-16s","<null>");
+            return IS_NULL;
+        case IS_FALSE : 
+            php_printf("%-16s","<false>");
+            return IS_FALSE;
+        case IS_TRUE : 
+            php_printf("%-16s","<true>");
+            return IS_TRUE;
+        case IS_LONG : 
+            php_printf("%-16ld", zv.value.lval);
+            return IS_LONG;
+        case IS_DOUBLE : 
+            php_printf("%-16g", zv.value.dval);
+            return IS_DOUBLE;
+        case IS_STRING: 
+            php_printf("%-16s", ZSTR_VAL(zv.value.str));
+            return IS_STRING;
+        case IS_ARRAY: 
+            php_printf("%-16s","<array>");
+            return IS_ARRAY;
+        case IS_OBJECT: 
+            php_printf("%-16s","<object>");
+            return IS_OBJECT;
+        case IS_RESOURCE: 
+            php_printf("%-16s","<resource>");
+            return IS_RESOURCE;
+        case IS_REFERENCE: 
+            php_printf("%-16s","<reference>");
+            return IS_REFERENCE;
+        case IS_CONSTANT: 
+            php_printf("%-16s","<constant>");
+            return IS_CONSTANT;
+        case IS_CALLABLE: 
+            php_printf("%-16s","<callable>");
+            return IS_CALLABLE;
+        case IS_INDIRECT: 
+            php_printf("%-16s","<indirect>");
+            return IS_INDIRECT;
+        case IS_PTR: 
+            php_printf("%-16s","<prt>");
+            return IS_PTR;
+        default: 
+            php_printf("%-16s","<unknown>");
+            return -1;
+    }
+}
+
+static void ngx_track_znode(unsigned int node_type, znode_op node, zend_op_array *op_array TSRMLS_DC)
+{
+    switch (node_type) {
+        case IS_UNDEF:
+            ngx_track_zval(*RT_CONSTANT_EX(op_array->literals, node));
+            break;
+        case IS_CONST:
+            ngx_track_zval(*RT_CONSTANT_EX(op_array->literals, node));
+            break;
+        case IS_TMP_VAR:
+            php_printf("~%-15d", EX_VAR_TO_NUM(node.var));
+            break;
+        case IS_VAR:
+            php_printf("$%-15d", EX_VAR_TO_NUM(node.var));
+            break;
+        case IS_CV:
+            php_printf("!%-15d", EX_VAR_TO_NUM(node.var));
+            break;
+        default:
+            php_printf("%-16s", " ");
+            break;
+    }
+}
+
+static void ngx_track_op_array(zend_op_array *op_array TSRMLS_DC)
+{
+    unsigned int i;
+    zend_op op;
+
+    php_printf("filename: '%s'\n", op_array->filename?ZSTR_VAL(op_array->filename):NULL);
+
+    if (op_array->scope) {
+        php_printf("function_name: %s::%s\n", ZSTR_VAL(op_array->scope->name), op_array->function_name?ZSTR_VAL(op_array->function_name):NULL);
+    }else {
+        php_printf("function_name: %s\n", op_array->function_name?ZSTR_VAL(op_array->function_name):NULL);
+    }
+
+    php_printf("    %-6s%-6s%-38s%-16s%-16s%-16s\n","id","line","opcode","op1","op2","result");
+    php_printf("    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+
+    for (i = 1; i < op_array->last; i++) {
+        op = op_array->opcodes[i];
+        php_printf("    %-6d%-6d%-38s", 
+            i, 
+            op.lineno, 
+            zend_get_opcode_name(op.opcode)
+            //op.op1_type,
+            //op.op2_type,
+            //op.result_type
+            );
+        ngx_track_znode(op.op1_type, op.op1, op_array TSRMLS_CC);
+        ngx_track_znode(op.op2_type, op.op2, op_array TSRMLS_CC);
+        ngx_track_znode(op.result_type, op.result, op_array TSRMLS_CC);
+        php_printf("\n");
+    }
+
+    php_printf("\n");
+}
+
+static int ngx_track_fe_wrapper(zval *el TSRMLS_DC, int num_args, va_list args, zend_hash_key *hash_key)
+{
+    return ngx_track_fe((zend_op_array *) Z_PTR_P(el) TSRMLS_CC, num_args, args, hash_key);
+}
+
+static int ngx_track_cle_wrapper (zval *el TSRMLS_DC)
+{
+    return ngx_track_cle((zend_class_entry *) Z_PTR_P(el) TSRMLS_CC);
+}
+
+/*static int ngx_check_fe_wrapper (zval *el, zend_bool *have_fe TSRMLS_DC)
+{
+    return ngx_check_fe((zend_op_array *) Z_PTR_P(el), have_fe TSRMLS_CC);
+}*/
+
+static int ngx_track_fe(zend_op_array *fe TSRMLS_DC, int num_args, va_list args, zend_hash_key *hash_key)
+{
+    if (fe->type == ZEND_USER_FUNCTION) {
+        ngx_track_op_array(fe TSRMLS_CC);
+    }
+
+    return ZEND_HASH_APPLY_KEEP;
+}
+
+static int ngx_track_cle(zend_class_entry *class_entry TSRMLS_DC)
+{
+    zend_class_entry *ce;
+    //zend_bool have_fe = 0;
+
+    ce = class_entry;
+
+    if (ce->type != ZEND_INTERNAL_CLASS) {
+        //zend_hash_apply_with_arguments(&ce->function_table, (apply_func_args_t) ngx_check_fe_wrapper, (int )&have_fe TSRMLS_CC);
+
+        //if (have_fe) {
+            zend_hash_apply_with_arguments(&ce->function_table TSRMLS_CC, (apply_func_args_t) ngx_track_fe_wrapper, 0);
+        //}
+    }
+
+    return ZEND_HASH_APPLY_KEEP; 
+}
+
+/*static int ngx_check_fe(zend_op_array *fe, zend_bool *have_fe TSRMLS_DC)
+{
+    if (fe->type == ZEND_USER_FUNCTION) {
+        *have_fe = 1;
+    }
+
+    return 0;
+}*/
+
+static inline zend_class_entry *ngx_get_called_scope(const zend_execute_data *e)
+{
+#if PHP_VERSION_ID < 70100
+    return e->called_scope;
+#else
+    return zend_get_called_scope((zend_execute_data *) e);
+#endif
+}
+
+static inline const char *ngx_get_executed_filename(void)
+{
+    zend_execute_data *ex = EG(current_execute_data);
+
+    while (ex && (!ex->func || !ZEND_USER_CODE(ex->func->type))) {
+        ex = ex->prev_execute_data;
+    }
+    if (ex) {
+        return ZSTR_VAL(ex->func->op_array.filename);
+    } else {
+        return zend_get_executed_filename();
+    }
+}
+
+static void ngx_function_name(zend_execute_data *execute_data)
+{
+    zend_execute_data *data;
+    zend_function *curr_func;
+    const char * cls;
+    const char * func;
+    const zend_op *opline;
+
+    data = EG(current_execute_data);
+
+    if (data) {
+        curr_func = data->func;
+        if (curr_func->common.function_name) {
+            func = curr_func->common.function_name->val;
+            cls = curr_func->common.scope ?
+                    curr_func->common.scope->name->val :
+                    (ngx_get_called_scope(data) ?
+                            ngx_get_called_scope(data)->name->val : NULL);
+            if (cls) {
+                php_printf("%s::%-20s", cls, func);
+            }else {
+                php_printf("%-30s", func);
+            }
+        }else {
+            if (data->prev_execute_data) {
+                opline = data->prev_execute_data->opline;
+            }else {
+                opline = data->opline;
+            }
+
+            if (opline) {
+                switch (opline->extended_value) {
+                    case ZEND_EVAL:
+                        php_printf("%-30s", "eval");
+                        break;
+                    case ZEND_INCLUDE:
+                        php_printf("%-30s", "include");
+                        break;
+                    case ZEND_REQUIRE:
+                        php_printf("%-30s", "require");
+                        break;
+                    case ZEND_INCLUDE_ONCE:
+                        php_printf("%-30s", "include_once");
+                        break;
+                    case ZEND_REQUIRE_ONCE:
+                        php_printf("%-30s", "require_once");
+                        break;
+                    default:
+                        php_printf("%-30s", "main");
+                        break;
+                }
+            }
+        }
+
+        //php_printf("\n");
+    }
+}
+
+static void ngx_stack_print_tab(ngx_uint_t depth, int flag)
+{
+    int i;
+    for (i = 0; i <= (int)depth; i++) {
+        /*if (i == 0) {
+            if (flag == 0) {
+                php_printf("   ");
+            }else {
+                php_printf("|---");
+            }
+        }else {
+            if (flag == 0) {
+                php_printf("    ");
+            }else {
+                php_printf("|---");
+            }
+        }*/
+        if (i == (int)depth) {
+            if (flag == 0) {
+                php_printf("*   ");
+            }else {
+                php_printf("|---");
+            }
+        }else {
+            php_printf("    ");
+        }
+    }
+}
+
+void ngx_execute_ex(zend_execute_data *execute_data TSRMLS_DC)
+{
+    int lineno;
+    const char *filename;
+
+    ngx_http_request_t *r = ngx_php_request;
+    ngx_http_php_ctx_t *ctx;
+    ctx = ngx_http_get_module_ctx(r, ngx_http_php_module);
+
+    if (ctx->output_type & OUTPUT_STACK) {
+        ctx->output_type = OUTPUT_CONTENT;
+
+        if (!ctx->stack_logo) {
+php_printf("    ____                __      \n");
+php_printf("   / __/_.._ ___  ___  / /__    \n");
+php_printf("  _\\ \\/_  __/ _ `/ _ `/  '_/    \n");
+php_printf(" /___/ /__/ \\_,\\/\\___/_/\\_\\     /ngx_php7_tracker\n");
+php_printf("                               /version: %s\n\n", NGX_HTTP_PHP_MODULE_VERSION);
+            ctx->stack_logo = 1;
+        }
+        //ngx_http_set_ctx(r, ctx, ngx_http_php_module);
+        
+        ngx_stack_print_tab(ctx->stack_depth,1);
+        //php_printf("%d", ctx->stack_depth);
+
+        ngx_function_name(execute_data);
+
+        lineno = zend_get_executed_lineno();
+
+        filename = ngx_get_executed_filename();
+        php_printf("%s line %-6d", filename, lineno);
+        php_printf("\n");
+
+        ctx->output_type = OUTPUT_STACK;
+        ctx->stack_depth += 1;
+
+        ngx_http_set_ctx(r, ctx, ngx_http_php_module);
+    }
+
+    struct timeval tv_start;
+    struct timeval tv_end;
+    gettimeofday(&tv_start, 0);
+
+    ori_execute_ex(execute_data TSRMLS_CC);
+
+    gettimeofday(&tv_end, 0);
+
+    if (ctx->output_type & OUTPUT_STACK) {
+        ctx->output_type = OUTPUT_CONTENT;
+        ctx->stack_depth -= 1;
+
+        //ngx_http_set_ctx(r, ctx, ngx_http_php_module);
+        ngx_stack_print_tab(ctx->stack_depth,0);
+        php_printf("real time: %f sec", (float)(((tv_end.tv_sec - tv_start.tv_sec) * 1000000) + (tv_end.tv_usec - tv_start.tv_usec))/1000000);
+        php_printf("\n");
+
+        ctx->output_type = OUTPUT_STACK;
+
+        ngx_http_set_ctx(r, ctx, ngx_http_php_module);
+    }
+}
+
+void ngx_execute_internal(zend_execute_data *execute_data, zval *return_value TSRMLS_DC)
+{
+    int lineno;
+    const char *filename;
+
+    ngx_http_request_t *r = ngx_php_request;
+    ngx_http_php_ctx_t *ctx;
+    ctx = ngx_http_get_module_ctx(r, ngx_http_php_module);
+
+    if (ctx->output_type & OUTPUT_STACK) {
+        ctx->output_type = OUTPUT_CONTENT;
+
+        if (!ctx->stack_logo) {
+php_printf("    ____                __      \n");
+php_printf("   / __/_.._ ___  ___  / /__    \n");
+php_printf("  _\\ \\/_  __/ _ `/ _ `/  '_/    \n");
+php_printf(" /___/ /__/ \\_,\\/\\___/_/\\_\\     /ngx_php7_tracker\n");
+php_printf("                               /version: %s\n\n", NGX_HTTP_PHP_MODULE_VERSION);
+            ctx->stack_logo = 1;
+        }
+        //ngx_http_set_ctx(r, ctx, ngx_http_php_module);
+        
+        ngx_stack_print_tab(ctx->stack_depth,1);
+        //php_printf("%d", ctx->stack_depth);
+
+        ngx_function_name(execute_data);
+
+        lineno = zend_get_executed_lineno();
+
+        filename = ngx_get_executed_filename();
+        php_printf("%s line %-6d", filename, lineno);
+        php_printf("\n");
+
+        ctx->output_type = OUTPUT_STACK;
+        ctx->stack_depth += 1;
+
+        ngx_http_set_ctx(r, ctx, ngx_http_php_module);
+    }
+
+    struct timeval tv_start;
+    struct timeval tv_end;
+    gettimeofday(&tv_start, 0);
+
+    execute_internal(execute_data, return_value);
+
+    gettimeofday(&tv_end, 0);
+
+    if (ctx->output_type & OUTPUT_STACK) {
+        ctx->output_type = OUTPUT_CONTENT;
+        ctx->stack_depth -= 1;
+
+        //ngx_http_set_ctx(r, ctx, ngx_http_php_module);
+        ngx_stack_print_tab(ctx->stack_depth,0);
+        php_printf("real time: %f sec", (float)(((tv_end.tv_sec - tv_start.tv_sec) * 1000000) + (tv_end.tv_usec - tv_start.tv_usec))/1000000);
+        php_printf("\n");
+        
+        ctx->output_type = OUTPUT_STACK;
+
+        ngx_http_set_ctx(r, ctx, ngx_http_php_module);
+        
+    }
+}
---
+++ src/php/php_ngx_track.h
@@ -0,0 +1,31 @@
+/**
+ *    Copyright(c) 2016 rryqszq4
+ *
+ *
+ */
+
+#ifndef _PHP_NGX_TRACK_H_
+#define _PHP_NGX_TRACK_H_
+
+#include <php.h>
+#include <SAPI.h>
+#include <php_main.h>
+#include <php_variables.h>
+#include <php_ini.h>
+#include <zend_ini.h>
+#include <zend_exceptions.h>
+#include <ext/standard/php_standard.h>
+#include <ext/standard/info.h>
+
+zend_op_array* (*ori_compile_file)(zend_file_handle* file_handle, int type TSRMLS_DC);
+zend_op_array* ngx_compile_file(zend_file_handle* file_handle, int type TSRMLS_DC);
+
+zend_op_array* (*ori_compile_string)(zval *source_string, char *filename TSRMLS_DC);
+zend_op_array* ngx_compile_string(zval *source_string, char *filename TSRMLS_DC);
+
+void (*ori_execute_ex)(zend_execute_data *execute_data TSRMLS_DC);
+void ngx_execute_ex(zend_execute_data *execute_data TSRMLS_DC);
+void ngx_execute_internal(zend_execute_data *execute_data, zval *return_value TSRMLS_DC);
+
+
+#endif
