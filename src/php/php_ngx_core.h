6be1abe3e43ce0991486d3dd45a7f9778b8287a3
quanzhao <279664262@163.com>
2017-10-30 23:32:30 +0800

    [src] Add ext php_ngx_core .

---
+++ src/php/php_ngx_core.h
@@ -0,0 +1,20 @@
+/**
+ *    Copyright(c) 2016-2017 rryqszq4
+ *
+ *
+ */
+
+#ifndef _PHP_NGX_CORE_H_
+#define _PHP_NGX_CORE_H_
+
+#include <ngx_http.h>
+
+#include <php.h>
+#include <php_ini.h>
+#include <ext/standard/info.h>
+
+PHP_METHOD(ngx, _exit);
+
+void php_ngx_core_init(int module_number TSRMLS_DC);
+
+#endif
