f559e99538fe53edac8ad534ffe7e6a567b9e9b7
quanzhao <279664262@163.com>
2017-11-24 00:51:10 +0800

    [src] Create php_ngx_generator file.

---
+++ src/php/php_ngx_generator.c
@@ -0,0 +1,7 @@
+/**
+ *    Copyright(c) 2016-2017 rryqszq4
+ *
+ *
+ */
+
+#include "php_ngx_generator.h"
---
+++ src/php/php_ngx_generator.h
@@ -0,0 +1,24 @@
+/**
+ *    Copyright(c) 2016-2017 rryqszq4
+ *
+ *
+ */
+
+#ifndef _PHP_NGX_GENERATOR_H_
+#define _PHP_NGX_GENERATOR_H_
+
+#include <ngx_http.h>
+
+#include <php.h>
+#include <php_ini.h>
+#include <ext/standard/info.h>
+
+PHP_METHOD(ngx_generator, run);
+PHP_METHOD(ngx_generator, next);
+
+PHP_METHOD(ngx_php, main);
+PHP_METHOD(ngx_php, next);
+
+void php_ngx_generator_init(int module_number TSRMLS_DC);
+
+#endif
