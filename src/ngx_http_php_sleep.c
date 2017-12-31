627da16132bf4200e842c16e1a8b15a6e87d229d
quanzhao <279664262@163.com>
2018-01-01 02:18:25 +0800

    [src] Add ngx_http_php_sleep .h and .c file.

---
+++ src/ngx_http_php_sleep.c
@@ -0,0 +1,75 @@
+/**
+ *    Copyright(c) 2016-2017 rryqszq4
+ *
+ *
+ */
+
+#include "ngx_http_php_sleep.h"
+
+static void ngx_http_php_sleep_cleanup(void *data);
+
+static void
+ngx_http_php_sleep_cleanup(void *data)
+{
+    ngx_http_request_t *r = data;
+    ngx_http_php_ctx_t *ctx;
+
+    ctx = ngx_http_get_module_ctx(r, ngx_http_php_module);
+
+    if (ctx == NULL) {
+        return ;
+    }
+
+    if (ctx->sleep.timer_set) {
+        ngx_del_timer(&ctx->sleep);
+        return ;
+    }
+}
+
+ngx_int_t
+ngx_http_php_sleep(ngx_http_request_t *r) 
+{
+    ngx_http_cleanup_t *cln;
+    ngx_http_php_ctx_t *ctx = ngx_http_get_module_ctx(r, ngx_http_php_module);
+
+    if (ctx == NULL) {
+        return NGX_HTTP_INTERNAL_SERVER_ERROR;
+    }
+
+    ctx->rewrite_phase = 1;
+    ctx->phase_status = NGX_AGAIN;
+
+    ngx_memzero(&ctx->sleep, sizeof(ngx_event_t));
+
+    ctx->sleep.handler = ngx_http_php_sleep_handler;
+    ctx->sleep.log = r->connection->log;
+    ctx->sleep.data = r;
+
+    ngx_add_timer(&ctx->sleep, (ngx_msec_t) ctx->delay_time);
+
+    cln = ngx_http_cleanup_add(r, 0);
+    if (cln == NULL) {
+        return NGX_ERROR;
+    }
+
+    cln->handler = ngx_http_php_sleep_cleanup;
+    cln->data = r;
+
+    r->keepalive = 0;
+
+    return NGX_OK;
+}
+
+void 
+ngx_http_php_sleep_handler(ngx_event_t *ev)
+{
+    ngx_http_request_t *r;
+
+    r = ev->data;
+
+    // todo uthread_resume
+
+    ngx_http_core_run_phases(r);
+}
+
+
---
+++ src/ngx_http_php_sleep.h
@@ -0,0 +1,15 @@
+/**
+ *    Copyright(c) 2016-2017 rryqszq4
+ *
+ *
+ */
+
+#ifndef NGX_HTTP_PHP_SLEEP_H
+#define NGX_HTTP_PHP_SLEEP_H
+
+#include "ngx_http_php_module.h"
+
+ngx_int_t ngx_http_php_sleep(ngx_http_request_t *r);
+void ngx_http_php_sleep_handler(ngx_event_t *ev);
+
+#endif
