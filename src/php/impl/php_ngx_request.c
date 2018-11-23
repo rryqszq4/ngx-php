485dd5e6d57b221e8a2d528716098068f4a516a0
Quan Zhao <rryqszq@gmail.com>
2018-11-23 11:01:07 +0800

    [src] Develop function ngx_request_*.

--- src/php/impl/php_ngx_request.c
+++ src/php/impl/php_ngx_request.c
@@ -9,6 +9,280 @@
 
 static zend_class_entry *php_ngx_request_class_entry;
 
+PHP_FUNCTION(ngx_request_method)
+{
+    ngx_http_request_t *r;
+
+    r = ngx_php_request;
+
+    
+    if (r->method == NGX_HTTP_GET) {
+        ZVAL_STRINGL(return_value, (char *)"GET", sizeof("GET") - 1);
+    }else if (r->method == NGX_HTTP_POST) {
+        ZVAL_STRINGL(return_value, (char *)"POST", sizeof("POST") - 1);
+    }else if (r->method == NGX_HTTP_PUT) {
+        ZVAL_STRINGL(return_value, (char *)"PUT", sizeof("PUT") - 1);
+    }else if (r->method == NGX_HTTP_HEAD) {
+        ZVAL_STRINGL(return_value, (char *)"HEAD", sizeof("HEAD") - 1);
+    }else if (r->method == NGX_HTTP_DELETE) {
+        ZVAL_STRINGL(return_value, (char *)"DELETE", sizeof("DELETE") - 1);
+    }
+}
+
+PHP_FUNCTION(ngx_request_document_root)
+{
+    ngx_http_request_t *r;
+    ngx_http_php_loc_conf_t *plcf;
+
+    r = ngx_php_request;
+    plcf = ngx_http_get_module_loc_conf(r, ngx_http_php_module);
+
+    ZVAL_STRINGL(return_value, (char *)plcf->document_root.data, plcf->document_root.len);
+}
+
+PHP_FUNCTION(ngx_request_document_uri)
+{
+    ngx_http_request_t *r;
+
+    r = ngx_php_request;
+
+    if ((r->uri.data)[r->uri.len-1] == '/') {
+        char *tmp_uri;
+        tmp_uri = emalloc(r->uri.len + 9 + 1);
+        ngx_cpystrn((u_char *)tmp_uri, (u_char *)r->uri.data, r->uri.len + 1);
+        strncat(tmp_uri, "index.php", 9);
+        ZVAL_STRINGL(return_value, (char *)tmp_uri, r->uri.len + 9);
+        efree(tmp_uri);
+    } else {
+        ZVAL_STRINGL(return_value, (char *)r->uri.data, r->uri.len);
+    }
+}
+
+PHP_FUNCTION(ngx_request_script_name)
+{
+    ngx_http_request_t *r;
+
+    r = ngx_php_request;
+
+    if ((r->uri.data)[r->uri.len-1] == '/') {
+        ZVAL_NULL(return_value);
+    } else {
+        ZVAL_STRINGL(return_value, (char *)r->uri.data, r->uri.len);
+    }
+}
+
+PHP_FUNCTION(ngx_request_script_filename)
+{
+    ngx_http_request_t *r;
+    ngx_http_php_loc_conf_t *plcf;
+
+    r = ngx_php_request;
+    plcf = ngx_http_get_module_loc_conf(r, ngx_http_php_module);
+
+    if ((r->uri.data)[r->uri.len-1] == '/'){
+        char *tmp_script;
+        tmp_script = emalloc(plcf->document_root.len + r->uri.len + 9 + 1);
+        ngx_cpystrn((u_char *)tmp_script, (u_char *)plcf->document_root.data, plcf->document_root.len+1);
+        strncat(tmp_script, (char *)r->uri.data, r->uri.len);
+        strncat(tmp_script, "index.php", 9);
+        ZVAL_STRINGL(return_value, (char *)tmp_script, plcf->document_root.len + r->uri.len + 9);
+        efree(tmp_script);
+    } else {
+        char *tmp_script;
+        tmp_script = emalloc(plcf->document_root.len + r->uri.len + 1);
+        ngx_cpystrn((u_char *)tmp_script, (u_char *)plcf->document_root.data, plcf->document_root.len+1);
+        strncat(tmp_script, (char *)r->uri.data, r->uri.len);
+        ZVAL_STRINGL(return_value, (char *)tmp_script, plcf->document_root.len + r->uri.len);
+        efree(tmp_script);
+    }
+}
+
+PHP_FUNCTION(ngx_request_query_string)
+{
+    ngx_http_request_t *r;
+
+    r = ngx_php_request;
+
+    if (r->args.len > 0){
+        ZVAL_STRINGL(return_value, (char *)r->args.data, r->args.len);
+    }else {
+        ZVAL_STRINGL(return_value, (char *)"", 0);
+    }
+}
+
+PHP_FUNCTION(ngx_request_uri)
+{
+    ngx_http_request_t *r;
+
+    r = ngx_php_request;
+
+    ZVAL_STRINGL(return_value, (char *)r->uri_start, strlen((char *)r->uri_start)-strlen((char *)r->uri_end));
+}
+
+PHP_FUNCTION(ngx_request_server_protocol)
+{
+    ngx_http_request_t *r;
+
+    r = ngx_php_request;
+
+    ZVAL_STRINGL(return_value, (char *)r->http_protocol.data, r->http_protocol.len);
+}
+
+PHP_FUNCTION(ngx_request_remote_addr)
+{
+    ngx_http_request_t *r;
+
+    r = ngx_php_request;
+
+    ZVAL_STRINGL(return_value, (char *)r->connection->addr_text.data, r->connection->addr_text.len);
+}
+
+PHP_FUNCTION(ngx_request_server_addr)
+{
+    ngx_http_request_t *r;
+    ngx_str_t  server_address;
+    u_char     server_addr[NGX_SOCKADDR_STRLEN];
+
+    r = ngx_php_request;
+    server_address.len = NGX_SOCKADDR_STRLEN;
+    server_address.data = server_addr;
+
+    if (ngx_connection_local_sockaddr(r->connection, &server_address, 0) != NGX_OK) {
+        ZVAL_NULL(return_value);    
+    }else {
+        ZVAL_STRINGL(return_value, (char *)server_address.data, server_address.len);
+    }
+}
+
+PHP_FUNCTION(ngx_request_remote_port)
+{
+    ngx_http_request_t *r;
+    ngx_uint_t port;
+    struct sockaddr_in  *sin;
+    //char *tmp_port;
+
+    r = ngx_php_request;
+    //tmp_port = emalloc(sizeof("65535") - 1);
+    sin = (struct sockaddr_in *) r->connection->local_sockaddr;
+    port = ntohs(sin->sin_port);
+    //ngx_sprintf((u_char *)tmp_port, "%ui", port);
+
+    //ZVAL_STRING(return_value, (char *)tmp_port);
+    ZVAL_LONG(return_value, port);
+    //efree(tmp_port);
+}
+
+PHP_FUNCTION(ngx_request_server_port)
+{
+    ngx_http_request_t *r;
+    ngx_uint_t port;
+    struct sockaddr_in  *sin;
+    //char *tmp_port;
+
+    r = ngx_php_request;
+    //tmp_port = emalloc(sizeof("65535") - 1);
+    sin = (struct sockaddr_in *) r->connection->local_sockaddr;
+    port = ntohs(sin->sin_port);
+    //ngx_sprintf((u_char *)tmp_port, "%ui", port);
+
+    //ZVAL_STRING(return_value, (char *)tmp_port);
+    ZVAL_LONG(return_value, port);
+    //efree(tmp_port);
+}
+
+PHP_FUNCTION(ngx_request_server_name)
+{
+    ngx_http_request_t *r;
+    ngx_http_core_srv_conf_t *cscf;
+
+    r = ngx_php_request;
+    cscf = ngx_http_get_module_srv_conf(r, ngx_http_core_module);
+
+    ZVAL_STRINGL(return_value, (char *)cscf->server_name.data, cscf->server_name.len);
+}
+
+PHP_FUNCTION(ngx_request_headers)
+{
+    ngx_http_request_t *r;
+    ngx_list_part_t *part;
+    ngx_table_elt_t *header;
+    ngx_uint_t i;
+
+    r = ngx_php_request;
+    part = &r->headers_in.headers.part;
+    header = part->elts;
+
+    array_init(return_value);
+
+    for (i = 0; /* void */; i++){
+        if (i >= part->nelts){
+            if (part->next == NULL){
+                break;
+            }
+            part = part->next;
+            header = part->elts;
+            i = 0;
+        }
+
+        if (ngx_strncasecmp(header[i].lowcase_key, (u_char *)"content-type", header[i].key.len) == 0){
+            add_assoc_stringl(return_value, "content_type", (char *)header[i].value.data, header[i].value.len);
+        }else {
+            add_assoc_stringl(return_value, "content_type", (char *)"", 0);
+        }
+
+        if (ngx_strncasecmp(header[i].lowcase_key, (u_char *)"content-length", header[i].key.len) == 0){
+            add_assoc_stringl(return_value, "content_length", (char *)header[i].value.data, header[i].value.len);
+        }else {
+            add_assoc_stringl(return_value, "content_length", (char *)"", 0);
+        }
+
+        if (ngx_strncasecmp(header[i].lowcase_key, (u_char *)"accept", header[i].key.len) == 0){
+            add_assoc_stringl(return_value, "accept", (char *)header[i].value.data, header[i].value.len);
+        }
+
+        if (ngx_strncasecmp(header[i].lowcase_key, (u_char *)"host", header[i].key.len) == 0){
+            add_assoc_stringl(return_value, "host", (char *)header[i].value.data, header[i].value.len);
+        }
+
+        if (ngx_strncasecmp(header[i].lowcase_key, (u_char *)"connection", header[i].key.len) == 0){
+            add_assoc_stringl(return_value, "connection", (char *)header[i].value.data, header[i].value.len);
+        }
+
+        if (ngx_strncasecmp(header[i].lowcase_key, (u_char *)"user-agent", header[i].key.len) == 0){
+            add_assoc_stringl(return_value, "user_agent", (char *)header[i].value.data, header[i].value.len);
+        }
+
+        if (ngx_strncasecmp(header[i].lowcase_key, (u_char *)"accept-encoding", header[i].key.len) == 0){
+            add_assoc_stringl(return_value, "accept_encoding", (char *)header[i].value.data, header[i].value.len);
+        }
+
+        if (ngx_strncasecmp(header[i].lowcase_key, (u_char *)"accept-language", header[i].key.len) == 0){
+            add_assoc_stringl(return_value, "accept_language", (char *)header[i].value.data, header[i].value.len);
+        }
+        
+        if (ngx_strncasecmp(header[i].lowcase_key, (u_char *)"referer", header[i].key.len) == 0){
+            add_assoc_stringl(return_value, "referer", (char *)header[i].value.data, header[i].value.len);
+        }
+
+        if (ngx_strncasecmp(header[i].lowcase_key, (u_char *)"pragma", header[i].key.len) == 0){
+            add_assoc_stringl(return_value, "pragma", (char *)header[i].value.data, header[i].value.len);
+        }
+
+        if (ngx_strncasecmp(header[i].lowcase_key, (u_char *)"cache-control", header[i].key.len) == 0){
+            add_assoc_stringl(return_value, "cache_control", (char *)header[i].value.data, header[i].value.len);
+        }
+
+        if (ngx_strncasecmp(header[i].lowcase_key, (u_char *)"upgrade-insecure-requests", header[i].key.len) == 0){
+            add_assoc_stringl(return_value, "upgrade_insecure_requests", (char *)header[i].value.data, header[i].value.len);
+        }
+
+        if (ngx_strncasecmp(header[i].lowcase_key, (u_char *)"cookie", header[i].key.len) == 0){
+            add_assoc_stringl(return_value, "cookie", (char *)header[i].value.data, header[i].value.len);
+        }
+
+    }
+}
+
 PHP_METHOD(ngx_request, method)
 {
     ngx_http_request_t *r;
