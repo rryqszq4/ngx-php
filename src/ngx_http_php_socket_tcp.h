563a11573c1839e6ac07b49bc44a56366137eece
Quan Zhao <quanzhao@sohu-inc.com>
2019-09-11 16:46:04 +0800

    [src] Rework ngx_http_php_socket_tcp

--- src/ngx_http_php_socket_tcp.h
+++ src/ngx_http_php_socket_tcp.h
@@ -29,4 +29,63 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef _NGX_HTTP_PHP_SOCKET_TCP_H_
 #define _NGX_HTTP_PHP_SOCKET_TCP_H_
 
+#include <ngx_core.h>
+#include <ngx_event.h>
+#include <ngx_event_connect.h>
+#include <ngx_http.h>
+
+
+/*typedef struct ngx_http_php_socket_pool_s {
+
+};*/
+
+typedef struct ngx_http_php_socket_tcp_upstream_s
+        ngx_http_php_socket_tcp_upstream_t;
+
+typedef void (*ngx_http_php_socket_tcp_upstream_handler_pt)(ngx_http_request_t *r, 
+    ngx_http_php_socket_tcp_upstream_t *u);
+
+struct ngx_http_php_socket_tcp_upstream_s {
+
+    ngx_http_php_socket_tcp_upstream_handler_pt     read_event_handler;
+    ngx_http_php_socket_tcp_upstream_handler_pt     write_event_handler;
+
+    ngx_msec_t      connect_timeout;
+    ngx_msec_t      read_timeout;
+    ngx_msec_t      write_timeout;
+
+    ngx_http_cleanup_pt     *cleanup;
+    ngx_http_request_t      *request;
+    ngx_peer_connection_t   peer;
+
+    ngx_http_upstream_resolved_t    *resolved;
+
+    ngx_buf_t       buffer;
+    size_t          buffer_size;
+    off_t           length;
+    unsigned        enabled_receive:1;
+
+    ngx_chain_t     *bufs_in;
+
+    ngx_chain_t     *busy_bufs;
+    ngx_chain_t     *free_bufs;
+
+    ngx_err_t       socket_errno;
+
+    ngx_int_t       (*input_filter)(void *data, ssize_t bytes);
+
+    size_t          request_len;
+    ngx_chain_t     *request_bufs;
+    
+
+};
+
+ngx_int_t ngx_http_php_socket_tcp_connect(ngx_http_request_t *r);
+void ngx_http_php_socket_tcp_close(ngx_http_request_t *r);
+
+ngx_int_t ngx_http_php_socket_tcp_send(ngx_http_request_t *r);
+ngx_int_t ngx_http_php_socket_tcp_recv(ngx_http_request_t *r);
+
+void ngx_http_php_socket_tcp_clear(ngx_http_request_t *r);
+
 #endif
