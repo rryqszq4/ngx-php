1c22875ea9d19fc0f856525dc4606093c7e4f3b6
Quan Zhao <quanzhao@sohu-inc.com>
2020-04-03 13:17:21 +0800

    [src] Add x-http-method-override in request header.

--- src/php/impl/php_ngx_request.c
+++ src/php/impl/php_ngx_request.c
@@ -310,6 +310,10 @@ PHP_FUNCTION(ngx_request_headers)
             add_assoc_stringl(return_value, "x_xsrf_token", (char *)header[i].value.data, header[i].value.len);
         }
 
+        if (ngx_strncasecmp(header[i].lowcase_key, (u_char *)"x-http-method-override", header[i].key.len) == 0){
+            add_assoc_stringl(return_value, "x_http_method_override", (char *)header[i].value.data, header[i].value.len);
+        }
+
     }
 }
 
