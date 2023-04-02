3f1f9ccfa068bc0277f6fc0726edb723f59086a8
Joanhey <joanhey@kumbiaphp.com>
2023-04-02 23:50:18 +0200

    Change nginx dir

--- .github/ngx-php/test.sh
+++ .github/ngx-php/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Copyright (c) 2016-2017, rryqszq4 <rryqszq@gmail.com>
-echo "ngx_php7 test ..."
-NGX_PATH=`pwd`'/build/nginx/sbin'
+echo "Ngx-php test ..."
+NGX_PATH=`pwd`'/nginx/sbin'
 ${NGX_PATH}/nginx -V
 export PATH=${NGX_PATH}:$PATH
-NGX_MODULE_PATH=`pwd`'/build/nginx/modules'
+NGX_MODULE_PATH=`pwd`'/nginx/modules'
 if [ -d "${NGX_MODULE_PATH}" ]; then
   for file in `\find ${NGX_MODULE_PATH} -name '*.so'`; do
     TEST_NGINX_LOAD_MODULES="${TEST_NGINX_LOAD_MODULES}${file} "
