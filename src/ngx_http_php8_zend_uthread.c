efc09103f0dba1015405cd28f97045662ac83a20
Joanhey <joanhey@kumbiaphp.com>
2023-04-11 18:27:22 +0200

    Fix to use _load_ & _store_

--- src/ngx_http_php8_zend_uthread.c
+++ src/ngx_http_php8_zend_uthread.c
@@ -402,8 +402,8 @@ cleanup_args:
         /* This flag is regularly checked while running user functions, but not internal
          * So see whether interrupt flag was set while the function was running... */
 #if (PHP_MAJOR_VERSION == 8 && PHP_MINOR_VERSION > 1) 
-        if (zend_atomic_bool_exchange_ex(&EG(vm_interrupt))) {
-            ZEND_ATOMIC_BOOL_INIT(&EG(vm_interrupt), false);
+        if (zend_atomic_bool_load_ex(&EG(vm_interrupt))) {
+            zend_atomic_bool_store_ex(&EG(vm_interrupt), false);
             if (zend_atomic_bool_load_ex(&EG(timed_out))) {
 #else
         if (EG(vm_interrupt)) {
