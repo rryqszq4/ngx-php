9ec9221276ab35ab82c4198ddb46b4fed544da51
Joanhey <joanhey@kumbiaphp.com>
2023-04-13 12:03:27 +0200

    Change paths and add setup job [ci only 22]

--- .github/ngx-php/compiler.sh
+++ .github/ngx-php/compiler.sh
@@ -88,13 +88,13 @@ echo "nginx install ..."
 if [ ! "${NGINX_MODULE}" = "DYNAMIC" ]; then
   ./configure --prefix=${NGINX_SRC_ROOT} \
               --with-ld-opt="-Wl,-rpath,$PHP_LIB" \
-              --add-module=../../ngx_php7/third_party/ngx_devel_kit \
-              --add-module=../../ngx_php7
+              --add-module=../../third_party/ngx_devel_kit \
+              --add-module=../..
 else
   ./configure --prefix=${NGINX_SRC_ROOT} \
               --with-ld-opt="-Wl,-rpath,$PHP_LIB" \
-              --add-dynamic-module=../../ngx_php7/third_party/ngx_devel_kit \
-              --add-dynamic-module=../../ngx_php7
+              --add-dynamic-module=../../third_party/ngx_devel_kit \
+              --add-dynamic-module=../..
 fi
 make
 make install
