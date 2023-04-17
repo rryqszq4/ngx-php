fa9bfda115adcfa449dc9d73d1af8f29af9353f0
Joanhey <joanhey@kumbiaphp.com>
2023-04-17 14:27:43 +0200

    Use github action setup-php only in 20.04 <compile <22

--- .github/ngx-php/compile-ngx.sh
+++ .github/ngx-php/compile-ngx.sh
@@ -28,11 +28,11 @@ cd nginx-${NGINX_SRC_VERSION}
 pwd
 
 #php-config
-#export PHP_CONFIG="/usr/bin/php-config$PHP_SRC_VERSION"
+export PHP_CONFIG="/usr/bin/php-config$PHP_SRC_VERSION"
 export PHP_LIB='/usr/lib'
-#export NGX_PHP_LIBS="`$PHP_CONFIG --ldflags` `$PHP_CONFIG --libs` -L$PHP_LIB -lphp$PHP_MAJOR_VERSION "
+export NGX_PHP_LIBS="`$PHP_CONFIG --ldflags` `$PHP_CONFIG --libs` -L$PHP_LIB -lphp$PHP_MAJOR_VERSION "
 
-php-config$PHP_SRC_VERSION
+php-config
 #ls ${PHP_SRC_ROOT}
 
 echo "Nginx install ..."
